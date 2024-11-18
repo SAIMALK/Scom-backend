edaee7cb6299b8591fdef12c26083ceddc91448b
SAIMALK <115073479+SAIMALK@users.noreply.github.com>
2024-11-18 19:35:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,25 +5,27 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cors from 'cors';
-
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
 app.use(cors({
-  origin: 'https://scomm.netlify.app', // Replace with your frontend URL
+  credentials: true,
+  origin: ['https://scomm.netlify.app', 'http://localhost:3000'], // Replace with your frontend URL
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
+app.use('/api/category', categoryRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
